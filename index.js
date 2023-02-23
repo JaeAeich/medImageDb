ba584ca1f573813d86b209f0fd605cfc214cf20a
JaeAeich <javedh@iitbhilai.ac.in>
2023-02-23 11:35:48 +0530

    DB connected

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const dbConnect = require("./dbConnect");
+const morgan = require("morgan");
+const cors = require("cors");
+
+dotenv.config("./.env");
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("common"));
+app.use(
+	cors({
+		credentials: true,
+		origin: "http://localhost:3000",
+	})
+);
+
+app.get("/", (req, res) => {
+	res.status(200).send("OK from Server");
+});
+
+const PORT = process.env.PORT || 4001;
+
+dbConnect();
+app.listen(PORT, () => {
+	console.log(`listening on port: ${PORT}`);
+});
